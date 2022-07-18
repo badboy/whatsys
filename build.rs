f5d0c59fc5632c87040ab23e35637285f66e1f25
Jan-Erik Rediger <janerik@fnordig.de>
2022-07-18 12:33:35 +0200

    Handle non-existence of TARGET or when it's empty

--- build.rs
+++ build.rs
@@ -2,13 +2,21 @@ extern crate cc;
 
 use std::env;
 
-fn main() {
-    let target = env::var("TARGET").unwrap();
-    let target_os = target.split('-').nth(2).unwrap();
+fn rmain() -> Option<()> {
+    // Missing? No idea what's going on.
+    let target = env::var("TARGET").ok()?;
+    // Not set? Definitely not windows then.
+    let target_os = target.split('-').nth(2)?;
 
     if target_os == "windows" {
         let mut builder = cc::Build::new();
         builder.file("c/windows.c");
         builder.compile("info");
     }
+
+    Some(())
+}
+
+fn main() {
+    let _ = rmain();
 }
